ae6bf6c0456402af77168b67e6b3a7a3a09b0dbf
TsarykOleg <tsaryk.oleg@gmail.com>
2021-10-31 21:09:50 +0200

    added numbers.html, numbers.js

---
+++ numbers.js
@@ -0,0 +1,38 @@
+// zero, one, two, three, four, five, six, seven, eight, nine
+let a = +prompt('Enter a number 0 - 9: ');
+switch (a) {
+    case 0:
+    alert('zero');
+    break;
+    case 1:
+    alert('one');
+    break;
+    case 2:
+    alert('two');
+    break;
+    case 3:
+    alert('three');
+    break;
+    case 4:
+    alert('four');
+    break;
+    case 5:
+    alert('five');
+    break;
+    case 6:
+    alert('six');
+    break;
+    case 7:
+    alert('seven');
+    break;
+    case 8:
+    alert('eight');
+    break;
+    case 9:
+    alert('nine');
+    break;
+    
+    default:
+    alert('You must enter a number from 0 to 9 !!!')
+}
+   
